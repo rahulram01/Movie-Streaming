6c7f89eaa1452b996ea8f4bb651849a0fcd08b6a
Rahul Ram P <98167864+rahulram01@users.noreply.github.com>
2024-11-06 15:21:42 +0530

    Add files via upload

---
+++ src/hooks/useMovieTrailer.jsx
@@ -0,0 +1,41 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { API_OPTIONS } from "../utils/constants";
+import { addTrailerVideo } from "../utils/moviesSlice";
+
+const useMovieTrailer = (movieId) => {
+  const dispatch = useDispatch();
+
+  const trailerVideo = useSelector((store) => store.movies.trailerVideo);
+
+  const getMovieVideos = async () => {
+    try {
+      const data = await fetch(
+        "https://api.themoviedb.org/3/movie/" +
+          movieId +
+          "/videos?language=en-US",
+        API_OPTIONS
+      );
+      const json = await data.json();
+
+      const filterData = json.results.filter(
+        (video) => video.type === "Trailer"
+      );
+      const trailer = filterData.length ? filterData[0] : json.results[0];
+      dispatch(addTrailerVideo(trailer));
+    } catch (error) {
+      console.error("Error fetching movie videos:", error);
+    }
+  };
+
+  useEffect(() => {
+    if (!trailerVideo) {
+      getMovieVideos();
+    }
+  }, [movieId, trailerVideo]);
+
+  // Return trailerVideo if needed
+  return trailerVideo;
+};
+
+export default useMovieTrailer;
